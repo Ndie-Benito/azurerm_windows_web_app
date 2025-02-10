f8311b3bd53b2c247bb58cedee3dc4d7885cbd10
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-10 18:45:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,28 @@
 
+
 # Crée un groupe de ressources
 resource "azurerm_resource_group" "rg" {
-  name     = "my-resource-group"
-  location = "West Europe"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 # Crée un plan App Service
 resource "azurerm_service_plan" "asp" {
-  name                = "my-app-service-plan"
+  name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "P1v2"  # Plan tarifaire
+  sku_name            = var.sku_name
   os_type             = "Windows"
 }
 
 # Crée une Web App Windows
 resource "azurerm_windows_web_app" "web_app" {
-  name                = "my-windows-web-app"
+  name                = var.web_app_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
-    always_on = true
+    always_on = true  # Garder l'application toujours active
   }
 }
