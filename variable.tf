4b7e212462e2fce4f656240cdeeaa60f2fa31c8d
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-10 18:34:16 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -11,3 +11,33 @@ variable "client_secret"{
 variable "tenant_id"{
   type=string
 }
+
+variable "resource_group_name" {
+  description = "Nom du groupe de ressources Azure"
+  type        = string
+  default     = "my-resource-group"
+}
+
+variable "location" {
+  description = "Localisation des ressources Azure"
+  type        = string
+  default     = "West Europe"
+}
+
+variable "app_service_plan_name" {
+  description = "Nom du plan App Service"
+  type        = string
+  default     = "my-app-service-plan"
+}
+
+variable "web_app_name" {
+  description = "Nom de la Web App"
+  type        = string
+  default     = "my-windows-web-app"
+}
+
+variable "sku_name" {
+  description = "Référence SKU du plan App Service"
+  type        = string
+  default     = "P1v2"
+}
