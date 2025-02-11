3703a4404d808965e515a69ec49f5ca0c77dd25a
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-10 19:30:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Crée un plan App Service
 resource "azurerm_service_plan" "asp" {
-  count               = 3  
+   
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
